f5add88e91328cc3d952cf432ab4be6a4887413f
= <=>
2023-08-28 00:49:55 +0800

    wzw1.0

--- rollingdesign/src/main.js
+++ rollingdesign/src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import ElementTiptapPlugin from 'element-tiptap'
+import 'element-plus/dist/index.css'
+import 'element-tiptap/lib/style.css'
+import './assets/css/main.css'
+import './assets/icons/iconfont.css'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(ElementPlus)
+app.use(ElementTiptapPlugin)
+app.mount('#app')
