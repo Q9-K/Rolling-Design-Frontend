bb60843df9c55aaf71c11fe17b85315b90e0f7e5
Q9-K <20374319@buaa.edu.cn>
2023-08-28 06:23:53 +0800

    路由修改

--- rollingdesign/src/App.vue
+++ rollingdesign/src/App.vue
@@ -7,35 +7,12 @@
 </template>
 
 <script setup>
-import {ref ,provide} from 'vue'
-// const app = createApp(App)
-
-// import axios from 'axios'
-// const axiosInstance = axios.create({
-//   baseURL: 'http://www.aamofe.top/api', // 设置基本 URL
-// });
-// provide('axios',axiosInstance)
-</script>
-
-<script setup>
 import { provide, onMounted } from 'vue'
 import axios from 'axios'
 const axiosInstance = axios.create({
   baseURL: 'http://www.aamofe.top/api', // 设置基本 URL
 });
 provide('axios', axiosInstance)
-// // onMounted(async () => {
-// let res = await axios.get('http://www.aamofe.top/api/team/all_teams', {
-//   headers: {
-//     Authorization: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2OTM1NDg0OTQsImlkIjoxfQ.HIgCyImqu4gytXnLAeE_7qkYd0INgcHeh6NHL_W3dMQ'
-//   }
-// })
-// console.log(res.data)
-
-//   console.log(res.data)
-// })
-
-// provide('test', 'test')
 </script>
 
 <style lang="scss">
