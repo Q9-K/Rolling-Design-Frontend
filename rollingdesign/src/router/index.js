5509eb60266b669d19ba9022c5b9933556bf06c7
= <=>
2023-08-28 12:22:31 +0800

    5.0

--- rollingdesign/src/router/index.js
+++ rollingdesign/src/router/index.js
@@ -28,15 +28,15 @@ const routes = [
     name: 'tiptap',
     component: () => import('../views/TipTap.vue')
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(
-      '../views/AboutView.vue')
-  }
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(
+  //     '../views/AboutView.vue')
+  // }
 ]
 
 const router = createRouter({
